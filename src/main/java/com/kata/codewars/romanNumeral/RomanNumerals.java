a70c34286e1228e65ecab1df095bfd3a13d93726
Arnaud JOYE <joye.arnaud@gmail.com>
2023-02-15 17:01:29 +0100

    kata_java18

--- src/main/java/com/kata/codewars/romanNumeral/RomanNumerals.java
+++ src/main/java/com/kata/codewars/romanNumeral/RomanNumerals.java
@@ -1,19 +1,27 @@
 package com.kata.codewars.romanNumeral;
 
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.lang.String.valueOf;
+import static java.util.stream.IntStream.iterate;
 
 public class RomanNumerals {
+    private static final Object[][] values = {{"I", 1}, {"IV", 4}, {"V", 5}, {"IX", 9}, {"X", 10}, {"XL", 40}, {"L", 50}, {"XC", 90}, {"C", 100}, {"CD", 400}, {"D", 500}, {"CM", 900}, {"M", 1000}};
 
-    public static class BiMap {
+    private static final class BiMap {
         Map<String, Integer> stringHashMap = new TreeMap<>();
         Map<Integer, String> intHashMap = new TreeMap<>();
 
-        public void put(String key, Integer value) {
+        public BiMap(Object[][] values) {
+            Map<String, Integer> map = Stream.of(values).collect(Collectors.toMap(data -> (String) data[0], data -> (Integer) data[1]));
+            map.forEach(this::put);
+        }
+
+        private void put(String key, Integer value) {
             if (!stringHashMap.containsValue(value) && !intHashMap.containsValue(key)) {
                 stringHashMap.put(key, value);
                 intHashMap.put(value, key);
@@ -24,43 +32,23 @@ public class RomanNumerals {
             return stringHashMap.get(s);
         }
 
-        public String convert(Integer i) {
+        public String convert(int i) {
             return intHashMap.get(i);
         }
 
-        public Integer biggest(Integer max) {
-            Map.Entry<Integer, String> integerStringEntry = intHashMap.entrySet().stream().filter(i -> i.getKey() < max).reduce((first, second) -> second).orElse(null);
+        public Integer biggest(int max) {
+            Entry<Integer, String> integerStringEntry = intHashMap.entrySet().stream().filter(i -> i.getKey() < max).reduce((first, second) -> second).orElse(null);
             return integerStringEntry == null ? 1 : integerStringEntry.getKey();
         }
     }
 
-    public static BiMap init() {
-        BiMap biMap = new BiMap();
-        biMap.put("I", 1);
-        biMap.put("IV", 4);
-        biMap.put("V", 5);
-        biMap.put("IX", 9);
-        biMap.put("X", 10);
-        biMap.put("XL", 40);
-        biMap.put("L", 50);
-        biMap.put("XC", 90);
-        biMap.put("C", 100);
-        biMap.put("CD", 400);
-        biMap.put("D", 500);
-        biMap.put("CM", 900);
-        biMap.put("M", 1000);
-
-        return biMap;
-    }
+    private static final BiMap biMap = new BiMap(values);
 
     public static String toRoman(int n) {
         StringBuilder stringBuilder = new StringBuilder();
 
-        BiMap biMap = init();
-
         int length = valueOf(n).length();
-        int[] ints = Integer.toString(n).chars().map(c -> c - '0').toArray();
-        for (int anInt : ints) {
+        for (int anInt : Integer.toString(n).chars().map(c -> c - '0').toArray()) {
             if (anInt != 0) {
                 int pow = (int) Math.pow(10, (length - 1));
                 appendC(stringBuilder, biMap, anInt * pow, pow);
@@ -73,24 +61,19 @@ public class RomanNumerals {
 
     private static void appendC(StringBuilder stringBuilder, BiMap biMap, int part, int pow) {
         String convert = biMap.convert(part);
-        boolean isInSpecialRule = (part + pow) % (5 * pow) == 0;
 
-        if (convert != null) {/*1; 5; 10; 50; 100; 500; 1000*/
+        if (convert != null) {
             stringBuilder.append(convert);
             return;
         }
-//        else if (isInSpecialRule) {/*4 => IV ; 9 => IX*/
-//            stringBuilder.append(biMap.convert(pow)).append(biMap.convert(part + pow));
-//            return;
-//        }
 
         int biggest = biMap.biggest(part);
         double xtime = (double) part / biggest;
         String bConverted = biMap.convert(biggest);
 
-        boolean isInteger = Math.abs(Math.floor(xtime) - xtime) < 1E-5;
+        boolean isInteger = (xtime % 1) == 0;
         if (isInteger)
-            stringBuilder.append(IntStream.iterate(0, y -> y < xtime, y -> y + 1).mapToObj(y -> bConverted).collect(Collectors.joining()));
+            stringBuilder.append(iterate(0, y -> y < xtime, y -> y + 1).mapToObj(y -> bConverted).collect(Collectors.joining()));
         else {
             int partLessBiggest = part - biggest;
             appendC(
@@ -100,11 +83,9 @@ public class RomanNumerals {
                     (int) Math.pow(10, valueOf(partLessBiggest).length() - 1)
             );
         }
-
     }
 
     public static int fromRoman(String romanNumeral) {
-        BiMap biMap = init();
         int res = 0;
 
         Integer integer = biMap.stringHashMap.get(romanNumeral);
@@ -113,9 +94,16 @@ public class RomanNumerals {
             char[] charArray = romanNumeral.toCharArray();
             for (int i = 0; i < charArray.length; i++) {
                 char c = charArray[i];
-                String s = i == charArray.length - 2 ? String.valueOf(c + charArray[i + 1]) : valueOf(c);
-                res += biMap.convert(s) != null ? biMap.convert(s) : biMap.convert(valueOf(c));
+                char cNext = i < charArray.length - 1 ? charArray[i + 1] : " ".charAt(0);
+
+                Integer convert = biMap.convert(String.valueOf(c) + cNext);
+                if (convert == null) res += biMap.convert(valueOf(c));
+                else {
+                    res += convert;
+                    i++;
+                }
             }
+
             return res;
         }
     }
--- src/test/java/com/kata/codewars/romanNumeral/RomanNumeralsTest.java
+++ src/test/java/com/kata/codewars/romanNumeral/RomanNumeralsTest.java
@@ -1,6 +1,13 @@
 package com.kata.codewars.romanNumeral;
 
 import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -8,7 +15,7 @@ public class RomanNumeralsTest {
 
     @Test
     public void testToRoman() throws Exception {
-        assertEquals ("I", RomanNumerals.toRoman(1), "1 converts to 'I'");
+        assertEquals("I", RomanNumerals.toRoman(1), "1 converts to 'I'");
         assertEquals("II", RomanNumerals.toRoman(2), "2 converts to 'II'");
         assertEquals("MLXXX", RomanNumerals.toRoman(1080), "1080 converts to 'MLXXX'");
         assertEquals("DLXX", RomanNumerals.toRoman(570), "1080 converts to 'DLXX'");
@@ -20,9 +27,59 @@ public class RomanNumeralsTest {
         assertEquals("MDCLXVI", RomanNumerals.toRoman(1666), "1666 converts to 'MDCLXVI'");
     }
 
-/*    @Test
+    @Test
     public void testFromRoman() throws Exception {
-        assertEquals(RomanNumerals.fromRoman("I"), 1, "'I' converts to 1");
-        assertEquals(RomanNumerals.fromRoman("II"), 2, "'II' converts to 2");
-    }*/
+        assertEquals(1, RomanNumerals.fromRoman("I"), "'I' converts to 1");
+        assertEquals(2, RomanNumerals.fromRoman("II"), "'II' converts to 2");
+        assertEquals(1666, RomanNumerals.fromRoman("MDCLXVI"), "'II' converts to 2");
+        assertEquals(44, RomanNumerals.fromRoman("XLIV"), "'XLIV' converts to 44");
+        assertEquals(3839, RomanNumerals.fromRoman("MMMDCCCXXXIX"), "'MMMDCCCXXXIX' converts to 3839");
+    }
+
+//    @Test
+    public void runBenchmarks() throws Exception {
+        Options options = new OptionsBuilder()
+                .include(this.getClass().getName() + ".*")
+                .mode(Mode.AverageTime)
+                .warmupTime(TimeValue.seconds(0))
+                .warmupIterations(0)
+                .threads(2)
+                .measurementIterations(1)
+                .forks(1)
+                .shouldFailOnError(true)
+                .shouldDoGC(true)
+                .build();
+
+        new Runner(options).run();
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void benchToRoman1() throws Exception {
+        RomanNumerals.toRoman(1);
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void benchToRoman2() throws Exception {
+        RomanNumerals.toRoman(2);
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void benchToRoman4() throws Exception {
+        RomanNumerals.toRoman(4);
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void benchToRoman1666() throws Exception {
+        RomanNumerals.toRoman(1666);
+    }
+
+//    @Benchmark
+//    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+//    public void benchToNumerals() throws Exception {
+//        RomanNumerals.fromRoman("I");
+//    }
 }
